0f6349b397edb10500c72a7d818e59fb599c41eb
Igor Pontes <igoppop@gmail.com>
2021-03-31 14:58:29 +0100

    Base files

--- vertexshader.glsl
+++
@@ -1,8 +0,0 @@
-#version 400 core
-layout (location = 0) in vec3 aPos;
-
-void main(){
-	gl_Position = vect4(aPos.x, aPos.y, aPox.z, 1.0);
-	// forth element = perspective division
-	// whatever we set gl_Position to will be used as the output of the vertex shader
-}
